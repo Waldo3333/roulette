5eea4da6059e6d0d01832ca7f38fd151d05541df
Charlie Gardere <charlie.gardere@gmail.com>
2024-12-01 22:22:00 +0100

    orga numero

--- script3.js
+++ script3.js
@@ -1,29 +1,299 @@
 /*-------------------------------------------------------- PARIER SUR UNE CASE !!!!!!*/
 //
 /*-------------------------------- Condition résultat (relou) */
-/*------------------------- Coté tiers */
-const upSide = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36];
-const middleSide = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35];
-const downSide = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34];
-/*------------------------- Bas tiers */
-const F12 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const S12 = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-const T12 = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
-/*------------------------- Rouge ou Noir */
-const black = [
-  2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35,
+
+// faire un tableau avec tous les chiffrers et leur correspondance !!!!!!!!!!
+const chiffre = [
+  1,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "downSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  2,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "middleSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  3,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "upSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  4,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "downSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  5,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "middleSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  6,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "upSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  7,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "downSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  8,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "middleSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  9,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "upSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  10,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "downSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  11,
+  {
+    color: "black",
+    isOdd: true,
+    tierDroite: "middleSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  12,
+  {
+    color: "red",
+    isOdd: false,
+    tierDroite: "upSide",
+    tierBas: "premTier",
+    half: "firstHalf",
+  },
+  13,
+  {
+    color: "black",
+    isOdd: true,
+    tierDroite: "downSide",
+    tierBas: "deuxTier",
+    half: "firstHalf",
+  },
+  14,
+  {
+    color: "red",
+    isOdd: false,
+    tierDroite: "middleSide",
+    tierBas: "deuxTier",
+    half: "firstHalf",
+  },
+  15,
+  {
+    color: "black",
+    isOdd: true,
+    tierDroite: "upSide",
+    tierBas: "deuxTier",
+    half: "firstHalf",
+  },
+  16,
+  {
+    color: "red",
+    isOdd: false,
+    tierDroite: "downSide",
+    tierBas: "deuxTier",
+    half: "firstHalf",
+  },
+  17,
+  {
+    color: "black",
+    isOdd: true,
+    tierDroite: "middleSide",
+    tierBas: "deuxTier",
+    half: "firstHalf",
+  },
+  18,
+  {
+    color: "red",
+    isOdd: false,
+    tierDroite: "upSide",
+    tierBas: "deuxTier",
+    half: "firstHalf",
+  },
+  19,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "downSide",
+    tierBas: "deuxTier",
+    half: "secondHalf",
+  },
+  20,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "middleSide",
+    tierBas: "deuxTier",
+    half: "secondHalf",
+  },
+  21,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "upSide",
+    tierBas: "deuxTier",
+    half: "secondHalf",
+  },
+  22,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "downSide",
+    tierBas: "deuxTier",
+    half: "secondHalf",
+  },
+  23,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "middleSide",
+    tierBas: "deuxTier",
+    half: "secondHalf",
+  },
+  24,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "upSide",
+    tierBas: "deuxTier",
+    half: "secondHalf",
+  },
+  25,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "downSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  26,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "middleSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  27,
+  {
+    color: "red",
+    isOdd: true,
+    tierDroite: "upSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  28,
+  {
+    color: "black",
+    isOdd: false,
+    tierDroite: "downSide",
+    tierBas: "pretroisTiermTier",
+    half: "secondHalf",
+  },
+  29,
+  {
+    color: "black",
+    isOdd: true,
+    tierDroite: "middleSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  30,
+  {
+    color: "red",
+    isOdd: false,
+    tierDroite: "upSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  31,
+  {
+    color: "black",
+    isOdd: true,
+    tierDroite: "downSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  32,
+  {
+    color: "red",
+    isOdd: false,
+    tierDroite: "middleSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  33,
+  {
+    color: "black",
+    isOdd: true,
+    tierDroite: "upSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  34,
+  {
+    color: "red",
+    isOdd: false,
+    tierDroite: "downSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  35,
+  {
+    color: "black",
+    isOdd: true,
+    tierDroite: "middleSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
+  36,
+  {
+    color: "red",
+    isOdd: false,
+    tierDroite: "upSide",
+    tierBas: "troisTier",
+    half: "secondHalf",
+  },
 ];
-const red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
-/*------------------------- Pair ou Impair */
-const odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 35, 37, 39, 31, 33, 35];
-const even = [
-  2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36,
-];
-/*------------------------- 1-18 / 19-36 */
-const one18 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-const nineteen36 = [
-  19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
-];
+
 /*------------------------------------ Placer sa mise*/
 
 let miseParie = {};
